ac048d39c62253b6696378b39a39097087d12f7a
khairul-islam-shaon <shaonkhairul@gmail.com>
2025-08-30 23:42:21 +0600

    demo code

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 import { finalVal } from "../../url";
 
 export const BASE_URL = () => {
-  let stage = "https://api-bracfish.adnarchive.com";
+  let stage = "https://api-bracfish.com";
   let prod = "https://api.bracfisheries.net";
 
   if (finalVal === "development") {
